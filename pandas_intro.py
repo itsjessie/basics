d871b56c487c0293266139bbc2df667db163fcdc
itsjessie <44765634+itsjessie@users.noreply.github.com>
2020-07-05 20:42:26 +1000

    Update pandas_intro.py

--- pandas_intro.py
+++ pandas_intro.py
@@ -1,3 +1,16 @@
 
 pd.head()
 df = pd.read_csv('path/to/file.csv')
+
+#get column
+df['column']
+
+#get single row
+df.iloc[2]
+#get row range
+df.loc[2:5]
+
+#get column and row
+#df.iloc [[rows],[column]]
+df.iloc[[0,1,],['A']] #get row 0 and 1 under column A
+
